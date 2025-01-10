26f872142e8c3affec9677134ab61c7773a20a6c
Mohamed4k <aaamohammed603@gmail.com>
2025-01-10 21:48:17 +0200

    Fixing

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -2942,26 +2942,7 @@ static void __sched_fork(unsigned long clone_flags, struct task_struct *p)
 	p->se.prev_sum_exec_runtime	= 0;
 	p->se.nr_migrations		= 0;
 	p->se.vruntime			= 0;
-	p->last_sleep_ts		= 0;
-	p->boost			= 0;
-	p->boost_expires		= 0;
-	p->boost_period			= 0;
-#ifdef CONFIG_SCHED_WALT
-	p->low_latency			= 0;
-#endif
-	p->last_sleep_ts		= 0;
-	p->boost			= 0;
-	p->boost_expires		= 0;
-	p->boost_period			= 0;
-#ifdef CONFIG_SCHED_WALT
-	p->low_latency			= 0;
-#endif
-#ifdef CONFIG_SCHED_BORE
-	sched_fork_bore(p);
-#endif // CONFIG_SCHED_BORE
-#ifdef CONFIG_SCHED_BORE
-	sched_fork_bore(p);
-#endif // CONFIG_SCHED_BORE
+	
 	INIT_LIST_HEAD(&p->se.group_node);
 
 #ifdef CONFIG_FAIR_GROUP_SCHED
